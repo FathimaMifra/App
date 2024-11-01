5d145606fade9e01e28350c753b099f1471d5077
fathimamifra <mifranijam@gmail.com>
2024-11-01 15:55:44 +0530

    add paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti corporis doloremque tenetur placeat eaque numquam! Odit et molestiae explicabo laudantium, sit iste laborum temporibus soluta. Placeat nobis at neque excepturi.</p>
 </body>
 </html>
